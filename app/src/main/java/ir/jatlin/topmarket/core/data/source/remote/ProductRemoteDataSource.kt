0f6220417247c7ec207f296eb532ea04f83a180c
MENasri23 <m.e26159487@gmail.com>
2022-05-20 12:23:29 -0700

    Define a new remote data source

---
+++ app/src/main/java/ir/jatlin/topmarket/core/data/source/remote/ProductRemoteDataSource.kt
@@ -0,0 +1,26 @@
+package ir.jatlin.topmarket.core.data.source.remote
+
+import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
+import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
+import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategoryDetails
+import retrofit2.Response
+
+interface ProductRemoteDataSource {
+
+    suspend fun findMovieDetailsById(
+        id: Int
+    ): Response<NetworkProductDetails>
+
+    suspend fun getProductsList(
+        page: Int,
+        pageSize: Int?,
+        filter: Map<String, String>?
+    ): Response<List<NetworkProduct>>
+
+    suspend fun getProductCategories(
+        page: Int,
+        pageSize: Int?,
+        filters: Map<String, String>?
+    ): Response<List<NetworkCategoryDetails>>
+
+}
--- app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
@@ -2,7 +2,6 @@ package ir.jatlin.topmarket.core.network.api
 
 import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
 import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
-import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategory
 import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategoryDetails
 import retrofit2.Response
 import retrofit2.http.GET
