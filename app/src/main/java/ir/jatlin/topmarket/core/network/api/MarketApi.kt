11e0c95cd90755d11bdfceb2c947118fb02749cb
MENasri23 <m.e26159487@gmail.com>
2022-05-20 10:08:00 -0700

    Define new market api and

---
+++ app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
@@ -0,0 +1,15 @@
+package ir.jatlin.topmarket.core.network.api
+
+import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
+import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface MarketApi {
+
+
+
+}
