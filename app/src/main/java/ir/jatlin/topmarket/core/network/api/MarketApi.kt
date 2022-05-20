153acecf660f5e0fb0ea0937d41e9fd512ec8fe0
MENasri23 <m.e26159487@gmail.com>
2022-05-20 10:11:25 -0700

    Add functionality to api
    Define method to Fetch a list of products from server
    Provide parameter for searching particular product if needed
    Provide method to Fetch product details

--- app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
@@ -10,6 +10,30 @@ import retrofit2.http.QueryMap
 
 interface MarketApi {
 
+    @GET(Route.PRODUCT_DETAILS)
+    suspend fun getProductDetails(
+        @Path(PARAMS.PRODUCT_ID) id: Int
+    ): Response<NetworkProductDetails>
+
+    @GET(Route.PRODUCTS)
+    suspend fun getProductsList(
+        @Query(PARAMS.PAGE) page: Int,
+        @Query(PARAMS.PER_PAGE) pageSize: Int?,
+        @QueryMap filters: Map<String, String>?
+    ): Response<List<NetworkProduct>>
+
+
+    private object Route {
+        const val PRODUCT_DETAILS = "/wp-json/wc/v3/products/{id}"
+        const val PRODUCTS = "/wp-json/wc/v3/products"
+    }
+
+    private object PARAMS {
+        const val PRODUCT_ID = "id"
+        const val PAGE = "page"
+        const val PER_PAGE = "per_page"
+
+    }
 
 
 }
