04e6773ea601db7264875ada9809cff8bf78a0e9
MENasri23 <m.e26159487@gmail.com>
2022-05-21 01:56:51 -0700

    Implement discovery parameters for products

---
+++ app/src/main/java/ir/jatlin/topmarket/core/domain/product/ProductDiscoverParameters.kt
@@ -0,0 +1,29 @@
+package ir.jatlin.topmarket.core.domain.product
+
+import ir.jatlin.topmarket.core.domain.param.DefaultDiscoverParameters
+
+class ProductDiscoverParameters : DefaultDiscoverParameters() {
+
+    enum class Order {
+        Asc, Desc
+    }
+
+    enum class OrderBY {
+        Date, Id, Include, Title, Slug, Price, Popularity, Rating
+    }
+
+    enum class StockStatus {
+        InStock, OutOfStock, onBackOrder
+    }
+
+    var order: Order = Order.Desc
+
+    var orderBy: OrderBY = OrderBY.Date
+
+    var categoryId: Int? = null
+
+    var tagId: Int? = null
+
+    var stockStatus: StockStatus? = null
+
+}
