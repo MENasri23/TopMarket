d494de26260e1236d4ca83af4234c16830a396e7
MENasri23 <m.e26159487@gmail.com>
2022-05-21 06:58:31 -0700

    Define application and add hilt entry point annotations

---
+++ app/src/main/java/ir/jatlin/topmarket/MarketApplication.kt
@@ -0,0 +1,14 @@
+package ir.jatlin.topmarket
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MarketApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
--- app/src/main/java/ir/jatlin/topmarket/ui/MainActivity.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import ir.jatlin.topmarket.R
 import ir.jatlin.topmarket.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
