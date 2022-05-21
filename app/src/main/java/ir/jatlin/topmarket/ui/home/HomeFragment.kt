92600b17bc7540f23b90a467325926e3231f23f4
MENasri23 <m.e26159487@gmail.com>
2022-05-21 03:46:53 -0700

    Convert fragment home to use data binding

--- app/src/main/java/ir/jatlin/topmarket/ui/home/HomeFragment.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/home/HomeFragment.kt
@@ -5,14 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ir.jatlin.topmarket.R
+import ir.jatlin.topmarket.databinding.FragmentHomeBinding
+import ir.jatlin.topmarket.ui.util.dataBindings
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val viewModel by viewModels<HomeViewModel>()
-
+    private val binding by dataBindings(FragmentHomeBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
 
     }
 
