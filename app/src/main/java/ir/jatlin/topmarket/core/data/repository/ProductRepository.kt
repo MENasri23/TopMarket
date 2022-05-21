a8911ffc047c911e4cf33322a6bb34aea3485438
MENasri23 <m.e26159487@gmail.com>
2022-05-21 00:23:05 -0700

    Change return types of product repository's methods to be flow base

--- app/src/main/java/ir/jatlin/topmarket/core/data/repository/ProductRepository.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/data/repository/ProductRepository.kt
@@ -3,23 +3,24 @@ package ir.jatlin.topmarket.core.data.repository
 import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
 import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
 import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategoryDetails
+import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     // TODO: Change return types of its method to external model
     suspend fun findProductDetailsById(
         id: Int
-    ): NetworkProductDetails
+    ): Flow<NetworkProductDetails>
 
     suspend fun getProductsList(
         page: Int,
         pageSize: Int?,
         filters: Map<String, String>?
-    ): List<NetworkProduct>
+    ): Flow<List<NetworkProduct>>
 
     suspend fun getProductCategories(
         page: Int,
         pageSize: Int?,
         filters: Map<String, String>?
-    ): List<NetworkCategoryDetails>
+    ): Flow<List<NetworkCategoryDetails>>
 
 }
