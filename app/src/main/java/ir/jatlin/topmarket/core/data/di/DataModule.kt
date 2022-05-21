0d7af459dd9083479356d74dbe4d1100a894e536
MENasri23 <m.e26159487@gmail.com>
2022-05-21 06:58:53 -0700

    Fix cyclic dependency injection bug

--- app/src/main/java/ir/jatlin/topmarket/core/data/di/DataModule.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/data/di/DataModule.kt
@@ -4,15 +4,18 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ir.jatlin.topmarket.core.data.repository.DefaultProductRepository
 import ir.jatlin.topmarket.core.data.repository.ProductRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface DataModule {
 
     @Binds
+    @Singleton
     fun bindsProductRepository(
-        productRepository: ProductRepository
+        productRepository: DefaultProductRepository
     ): ProductRepository
 
 }
