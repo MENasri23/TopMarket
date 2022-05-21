b0a42fa90a469103ec7ec6779f6e52c92c5325a0
MENasri23 <m.e26159487@gmail.com>
2022-05-21 07:03:43 -0700

    Add hilt annotation and test the result retrieved from view model

--- app/src/main/java/ir/jatlin/topmarket/ui/home/HomeFragment.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/home/HomeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import ir.jatlin.topmarket.R
 import ir.jatlin.topmarket.databinding.FragmentHomeBinding
 import ir.jatlin.topmarket.ui.util.dataBindings
+import ir.jatlin.topmarket.ui.util.repeatOnViewLifecycle
 
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val viewModel by viewModels<HomeViewModel>()
@@ -17,6 +20,15 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewModel = viewModel
+
+
+        repeatOnViewLifecycle {
+            viewModel.homeUiState.collect {
+                it?.categorizedProducts.let { result ->
+                    binding.tvShow.text = result?.get(0).toString()
+                }
+            }
+        }
 
     }
 
