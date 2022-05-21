c782d82151badb2bbef6faacbd3e2dd8f21994de
MENasri23 <m.e26159487@gmail.com>
2022-05-21 00:16:54 -0700

    Create Resource wrapper class to manage of retrieving data from data layer

---
+++ app/src/main/java/ir/jatlin/topmarket/core/shared/Resource.kt
@@ -0,0 +1,35 @@
+package ir.jatlin.topmarket.core.shared
+
+import ir.jatlin.topmarket.core.shared.fail.ErrorCause
+
+/**
+ * A generic class that holds a value with its loading status.
+ * @param <T>
+ */
+sealed class Resource<out R>(
+    val data: R? = null,
+    val cause: ErrorCause? = null
+) {
+
+    class Success<out T>(data: T) : Resource<T>(data)
+    class Loading<out T>(data: T? = null) : Resource<T>(data)
+    class Error(cause: ErrorCause) : Resource<Nothing>(cause = cause)
+
+    override fun toString(): String {
+        return when (this) {
+            is Success -> "Success[data=$data]"
+            is Error -> "Error[exception=$cause]"
+            is Loading -> "Loading"
+        }
+    }
+
+    companion object {
+        fun <T> success(data: T): Resource<T> = Success(data)
+        fun <T> loading(data: T? = null): Resource<T> = Loading(data)
+        fun <T> error(cause: ErrorCause): Resource<T> = Error(cause)
+    }
+
+}
+
+val <T> Resource<T>.isSuccess
+    get() = this is Resource.Success && data != null
