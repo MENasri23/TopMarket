2375447b5f5e86ef6bfe4c32eed01bfd1f8e790d
MENasri23 <m.e26159487@gmail.com>
2022-05-22 01:45:40 -0700

    feat(viewHolder): Define new view holder to show products

---
+++ app/src/main/java/ir/jatlin/topmarket/ui/home/ProductViewHolder.kt
@@ -0,0 +1,17 @@
+package ir.jatlin.topmarket.ui.home
+
+import ir.jatlin.topmarket.databinding.ProductCategoryItemViewBinding
+
+class ProductViewHolder(
+    private val binding: ProductCategoryItemViewBinding
+) : BaseViewHolder<HomeItem.ProductItem>(binding) {
+
+    init {
+        // TODO: Set listeners and other configuration if needed
+    }
+
+    override fun bind(item: HomeItem.ProductItem) {
+        // TODO: bind view with item
+    }
+
+}
