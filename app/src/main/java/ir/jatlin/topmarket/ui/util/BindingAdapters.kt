8fdb1a81b8c9f16e32ad6ad5636122334b4d4a89
MENasri23 <m.e26159487@gmail.com>
2022-05-22 03:26:02 -0700

    feat(home): Define top level function to Load  image with glide

--- app/src/main/java/ir/jatlin/topmarket/ui/util/BindingAdapters.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/util/BindingAdapters.kt
@@ -4,12 +4,21 @@ import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import ir.jatlin.topmarket.R
 
 
-fun ImageView.loadFromUrl(url: String?, @DrawableRes placeholder: Int) {
+fun ImageView.loadFromUrl(
+    url: String?,
+    @DrawableRes placeholder: Int = R.drawable.loading_animation
+) {
     Glide.with(context)
         .load(url)
-        .placeholder(placeholder)
+        .apply(
+            RequestOptions()
+                .placeholder(placeholder)
+                .error(R.drawable.ic_broken_image)
+        )
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(this)
 }
