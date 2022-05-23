615dfd483a8821e77162a428357914d3724c4329
MENasri23 <m.e26159487@gmail.com>
2022-05-23 05:46:14 -0700

    feat(product-details): Get product id by savedStateHandle

--- app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
@@ -2,11 +2,23 @@ package ir.jatlin.topmarket.ui.productdetails
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
+import ir.jatlin.topmarket.ui.util.stateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
 class ProductDetailsViewModel @Inject constructor(
+
     private val state: SavedStateHandle
 ) : ViewModel() {
 
-    // todo: Implement its behavior
+    private val productId = state.getLiveData<Int>("productId").asFlow()
+
+
 }
+
+
