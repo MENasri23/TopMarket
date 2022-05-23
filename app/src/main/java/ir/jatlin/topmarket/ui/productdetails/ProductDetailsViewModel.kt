3fcdd4d23b6a9af5ce9ea35c4f8e7db44ecb45b3
MENasri23 <m.e26159487@gmail.com>
2022-05-23 06:16:52 -0700

    feat(product-details): Get product details

--- app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
@@ -3,21 +3,21 @@ package ir.jatlin.topmarket.ui.productdetails
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asFlow
-import androidx.lifecycle.viewModelScope
+import ir.jatlin.topmarket.core.domain.product.FetchProductDetailsUseCase
 import ir.jatlin.topmarket.ui.util.stateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
 class ProductDetailsViewModel @Inject constructor(
-
-    private val state: SavedStateHandle
+    private val fetchProductDetails: FetchProductDetailsUseCase,
+    state: SavedStateHandle
 ) : ViewModel() {
 
     private val productId = state.getLiveData<Int>("productId").asFlow()
 
+    val productDetails = stateFlow {
+        productId.map { fetchProductDetails(it) }
+    }
 
 }
 
