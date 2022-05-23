7d370c115cbde8a7a49718ba00400c5316599f2d
MENasri23 <m.e26159487@gmail.com>
2022-05-23 06:25:41 -0700

    feat(product-details): Collect product details in fragment

--- app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsFragment.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import ir.jatlin.topmarket.R
+import ir.jatlin.topmarket.core.shared.Resource
 import ir.jatlin.topmarket.databinding.FragmentProductDetailsBinding
 import ir.jatlin.topmarket.ui.util.dataBindings
+import ir.jatlin.topmarket.ui.util.repeatOnViewLifecycleOwner
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProductDetailsFragment : Fragment(R.layout.fragment_product_details) {
@@ -20,7 +23,31 @@ class ProductDetailsFragment : Fragment(R.layout.fragment_product_details) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
 
+        initViews()
 
+    }
+
+    private fun initViews() = binding.apply {
+
+    }
+
+    private fun collectUiStates() = repeatOnViewLifecycleOwner {
+        launch {
+            collectProductDetails()
+        }
+    }
+
+    private suspend fun collectProductDetails() {
+        viewModel.productDetails.collect { state ->
+            when (state) {
+                is Resource.Error -> {}
+                is Resource.Loading -> {}
+                is Resource.Success -> {
+                    val productDetails = state.data!!
+                    viewModel.updateUiStatesWith(productDetails)
+                }
+            }
+        }
     }
 
 
--- app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/productdetails/ProductDetailsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asFlow
 import ir.jatlin.topmarket.core.domain.product.FetchProductDetailsUseCase
+import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
 import ir.jatlin.topmarket.ui.util.stateFlow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -12,13 +13,16 @@ class ProductDetailsViewModel @Inject constructor(
     private val fetchProductDetails: FetchProductDetailsUseCase,
     state: SavedStateHandle
 ) : ViewModel() {
-
     private val productId = state.getLiveData<Int>("productId").asFlow()
 
     val productDetails = stateFlow {
         productId.map { fetchProductDetails(it) }
     }
 
+
+    fun updateUiStatesWith(productDetails: NetworkProductDetails) {
+        // TODO: set states with current product
+    }
 }
 
 
