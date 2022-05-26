fe384c7526254639a4ddc15e8530c5d071fff872
MENasri23 <m.e26159487@gmail.com>
2022-05-25 23:24:54 -0700

    feat(details): Get layout inflater from the parent in view holder creator

--- app/src/main/java/ir/jatlin/topmarket/ui/viewholder/ViewHolderCreator.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/viewholder/ViewHolderCreator.kt
@@ -1,7 +1,12 @@
 package ir.jatlin.topmarket.ui.viewholder
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
 interface ViewHolderCreator<T> {
+
     fun createFrom(parent: ViewGroup, viewType: Int): BaseViewHolder<T>
+
+    fun layoutInflater(parent: ViewGroup): LayoutInflater =
+        LayoutInflater.from(parent.context)
 }
