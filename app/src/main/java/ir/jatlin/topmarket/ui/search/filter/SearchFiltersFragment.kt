def71b010b4b914b2e521fce6c9e8c5de3de6f15
MENasri23 <m.e26159487@gmail.com>
2022-05-29 23:45:39 -0700

    feat(filter) : Define new fragment to apply filter on products

---
+++ app/src/main/java/ir/jatlin/topmarket/ui/search/filter/SearchFiltersFragment.kt
@@ -0,0 +1,7 @@
+package ir.jatlin.topmarket.ui.search.filter
+
+import androidx.fragment.app.Fragment
+import ir.jatlin.topmarket.R
+
+class SearchFiltersFragment : Fragment(R.layout.fragment_search_filters) {
+}
