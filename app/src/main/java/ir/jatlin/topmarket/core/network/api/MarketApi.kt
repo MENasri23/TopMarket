0d4fd0058eb4fb4cac3cd5b9c988f57cd72c1698
MENasri23 <m.e26159487@gmail.com>
2022-06-01 08:33:27 -0700

    feat(cart-api): Add new methods to get and update the order has been created

--- app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
@@ -1,6 +1,7 @@
 package ir.jatlin.topmarket.core.network.api
 
 import ir.jatlin.topmarket.core.network.model.costumer.CustomerNetwork
+import ir.jatlin.topmarket.core.network.model.order.OrderNetwork
 import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
 import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
 import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategoryDetails
@@ -29,21 +30,37 @@ interface MarketApi {
     ): Response<List<NetworkCategoryDetails>>
 
 
-    @POST(Route.CUSTOMER_NEW)
-    suspend fun createCustomer(
-        @Body customerNetwork: CustomerNetwork
-    ): Response<CustomerNetwork>
-
     @GET(Route.COSTUMER)
     suspend fun getCustomer(
         @Path("id") id: Int
+    ): Response<CustomerNetwork>
+
+    @POST(Route.CUSTOMER_NEW)
+    suspend fun createCustomer(
+        @Body customerNetwork: CustomerNetwork
     ): Response<CustomerNetwork>
 
     @PUT(Route.COSTUMER)
     suspend fun updateCustomer(
         @Path("id") id: Int,
         @Body customer: CustomerNetwork
-    )
+    ): Response<CustomerNetwork>
+
+    @GET(Route.ORDER)
+    suspend fun getOrder(
+        @Path("id") id: Int
+    ): Response<OrderNetwork>
+
+    @POST(Route.ORDER_NEW)
+    suspend fun createOrder(
+        @Body orderNetwork: OrderNetwork
+    ): Response<OrderNetwork>
+
+    @PUT(Route.ORDER)
+    suspend fun updateOrder(
+        @Path("id") id: Int,
+        @Body order: OrderNetwork
+    ): Response<OrderNetwork>
 
     private object Route {
         private const val PREFIX = "/wp-json/wc/v3"
@@ -54,6 +71,10 @@ interface MarketApi {
         /* customer */
         const val CUSTOMER_NEW = "$PREFIX/customers"
         const val COSTUMER = "$PREFIX/customers/{id}"
+
+        /* Orders */
+        const val ORDER_NEW = "$PREFIX/orders"
+        const val ORDER = "$PREFIX/{id}"
     }
 
     private object PARAMS {
