4575ba7db180a362249003e27aa5abf63fb7ba73
MENasri23 <m.e26159487@gmail.com>
2022-06-01 11:11:05 -0700

    feat(cart-repo): Bind order retrofit remote data source to be injected as remote data source

--- app/src/staging/java/ir/jatlin/topmarket/core/data/source/di/SourceModule.kt
+++ app/src/staging/java/ir/jatlin/topmarket/core/data/source/di/SourceModule.kt
@@ -8,6 +8,8 @@ import ir.jatlin.topmarket.core.data.source.remote.ProductRemoteDataSource
 import ir.jatlin.topmarket.core.data.source.remote.ProductRetrofitDataSource
 import ir.jatlin.topmarket.core.data.source.remote.customer.CustomerRemoteDataSource
 import ir.jatlin.topmarket.core.data.source.remote.customer.CustomerRetrofitDataSource
+import ir.jatlin.topmarket.core.data.source.remote.order.OrderRemoteDataSource
+import ir.jatlin.topmarket.core.data.source.remote.order.OrderRetrofitDataSource
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,4 +24,9 @@ interface SourceModule {
     fun bindsCustomerRemoteDataSource(
         remoteDataSource: CustomerRetrofitDataSource
     ): CustomerRemoteDataSource
+
+    @Binds
+    fun bindsOrderRemoteDataSource(
+        remoteDataSource: OrderRetrofitDataSource
+    ): OrderRemoteDataSource
 }
