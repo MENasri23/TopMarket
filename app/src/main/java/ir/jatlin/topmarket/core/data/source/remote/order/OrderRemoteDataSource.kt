67914dbd40f91d6117512ff7e3d329f001a0ff3c
MENasri23 <m.e26159487@gmail.com>
2022-06-01 08:39:31 -0700

    feat(cart-source): Create new remote data source for orders

---
+++ app/src/main/java/ir/jatlin/topmarket/core/data/source/remote/order/OrderRemoteDataSource.kt
@@ -0,0 +1,12 @@
+package ir.jatlin.topmarket.core.data.source.remote.order
+
+import ir.jatlin.topmarket.core.network.model.order.OrderNetwork
+
+interface OrderRemoteDataSource {
+
+    suspend fun findOrderById(orderId: Int): OrderNetwork
+
+    suspend fun createOrder(orderNetwork: OrderNetwork): OrderNetwork
+
+    suspend fun updateOrder(order: OrderNetwork): OrderNetwork
+}
