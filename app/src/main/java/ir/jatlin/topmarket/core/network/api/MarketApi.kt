16e0bd7a8baaab93924ad3bd958152bda01eddd1
MENasri23 <m.e26159487@gmail.com>
2022-06-01 08:24:40 -0700

    feat(cart-api): Add route for creating new customer

--- app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/network/api/MarketApi.kt
@@ -1,13 +1,11 @@
 package ir.jatlin.topmarket.core.network.api
 
+import ir.jatlin.topmarket.core.network.model.costumer.CustomerNetwork
 import ir.jatlin.topmarket.core.network.model.product.NetworkProduct
 import ir.jatlin.topmarket.core.network.model.product.NetworkProductDetails
 import ir.jatlin.topmarket.core.network.model.product.category.NetworkCategoryDetails
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
-import retrofit2.http.QueryMap
+import retrofit2.http.*
 
 interface MarketApi {
 
@@ -31,11 +29,21 @@ interface MarketApi {
     ): Response<List<NetworkCategoryDetails>>
 
 
+    @POST(Route.CUSTOMER_NEW)
+    suspend fun createCustomer(
+        @Body customerNetwork: CustomerNetwork
+    ) : Response<CustomerNetwork>
+
+
     private object Route {
         private const val PREFIX = "/wp-json/wc/v3"
         const val PRODUCT_DETAILS = "$PREFIX/products/{id}"
         const val PRODUCTS = "$PREFIX/products"
         const val PRODUCTS_CATEGORIES = "$PREFIX/products/categories"
+
+        /* customer */
+        const val CUSTOMER_NEW = "$PREFIX/customers"
+        const val COSTUMER = "$PREFIX/customers/{id}"
     }
 
     private object PARAMS {
