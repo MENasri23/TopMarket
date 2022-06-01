6efaf590da1c0f5e38212899aea2e923b18b8594
MENasri23 <m.e26159487@gmail.com>
2022-06-01 11:08:55 -0700

    feat(cart-repo): Define a repository for the order

---
+++ app/src/main/java/ir/jatlin/topmarket/core/data/repository/order/OrderRepository.kt
@@ -0,0 +1,13 @@
+package ir.jatlin.topmarket.core.data.repository.order
+
+import ir.jatlin.topmarket.core.model.order.Order
+
+interface OrderRepository {
+
+    suspend fun findOrderById(orderId: Int): Order
+
+    suspend fun createOrder(order: Order): Order
+
+    suspend fun updateOrder(order: Order): Order
+
+}
