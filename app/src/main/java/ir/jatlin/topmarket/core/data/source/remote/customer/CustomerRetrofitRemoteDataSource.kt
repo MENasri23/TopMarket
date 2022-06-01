235564df719cf2ede87287931d5564f71889fa38
MENasri23 <m.e26159487@gmail.com>
2022-06-01 08:42:43 -0700

    feat(cart-source): Implement customer remote data source

---
+++ app/src/main/java/ir/jatlin/topmarket/core/data/source/remote/customer/CustomerRetrofitRemoteDataSource.kt
@@ -0,0 +1,29 @@
+package ir.jatlin.topmarket.core.data.source.remote.customer
+
+import ir.jatlin.data.source.remote.ResponseConverter
+import ir.jatlin.topmarket.core.network.api.MarketApi
+import ir.jatlin.topmarket.core.network.model.costumer.CustomerNetwork
+import javax.inject.Inject
+
+class CustomerRetrofitRemoteDataSource @Inject constructor(
+    private val marketApi: MarketApi,
+    private val convertResponse: ResponseConverter
+) : CustomerRemoteDataSource {
+
+    override suspend fun findCustomerById(customerId: Int): CustomerNetwork {
+        return convertResponse(marketApi.getCustomer(customerId))
+    }
+
+    override suspend fun createCustomer(customerNetwork: CustomerNetwork): CustomerNetwork {
+        return convertResponse((marketApi.createCustomer(customerNetwork)))
+    }
+
+    override suspend fun updateCustomer(customer: CustomerNetwork): CustomerNetwork {
+        return convertResponse(
+            marketApi.updateCustomer(
+                id = customer.id,
+                customer = customer
+            )
+        )
+    }
+}
