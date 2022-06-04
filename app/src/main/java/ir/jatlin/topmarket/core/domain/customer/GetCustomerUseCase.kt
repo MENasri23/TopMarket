4d9ee8c497960534311130e5e53e00eae59ecc6d
MENasri23 <m.e26159487@gmail.com>
2022-06-04 07:49:25 -0700

    feat(view): Create a view for adding or removing items from cart

--- app/src/main/java/ir/jatlin/topmarket/core/domain/customer/GetCustomerUseCase.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/domain/customer/GetCustomerUseCase.kt
@@ -1,20 +1,22 @@
 package ir.jatlin.topmarket.core.domain.customer
 
 import ir.jatlin.topmarket.core.data.repository.customer.CustomerRepository
+import ir.jatlin.topmarket.core.domain.CoroutineUseCase
 import ir.jatlin.topmarket.core.domain.FlowUseCase
 import ir.jatlin.topmarket.core.model.user.Customer
 import ir.jatlin.topmarket.core.shared.fail.ErrorHandler
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetCustomerUseCase @Inject constructor(
     private val customerRepository: CustomerRepository,
     errorHandler: ErrorHandler,
     dispatcher: CoroutineDispatcher
-) : FlowUseCase<Int, Customer>(errorHandler, dispatcher) {
+) : CoroutineUseCase<Int, Customer>(errorHandler, dispatcher) {
 
-    override fun execute(params: Int): Flow<Customer> {
+    override suspend fun execute(params: Int): Customer {
         return customerRepository.findCustomerById(params)
     }
 }
