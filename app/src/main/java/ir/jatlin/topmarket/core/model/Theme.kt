baf90ed9191e643e1f353c0837af19922e64e732
MENasri23 <m.e26159487@gmail.com>
2022-06-06 03:29:22 -0700

    feat(theme): Define new enum represents app theme mode

---
+++ app/src/main/java/ir/jatlin/topmarket/core/model/Theme.kt
@@ -0,0 +1,13 @@
+package ir.jatlin.topmarket.core.model
+
+enum class Theme(val prefsKey: String) {
+    LIGHT("light"),
+    DARK("dark"),
+    SYSTEM("system"),
+    BATTERY_SAVER("battery_saver");
+
+    companion object {
+        fun fromPreferenceKey(prefsKey: String): Theme? =
+            values().firstOrNull { it.prefsKey == prefsKey }
+    }
+}
