cf0bbdfd8c1fd17608b0bd201f36144ab1e0e47c
MENasri23 <m.e26159487@gmail.com>
2022-06-11 08:27:23 -0700

    feat(purchase-view): Create new view model for cart fragment

--- app/src/main/java/ir/jatlin/topmarket/ui/purchase/cart/CartFragment.kt
+++ app/src/main/java/ir/jatlin/topmarket/ui/purchase/cart/CartFragment.kt
@@ -1,7 +1,29 @@
 package ir.jatlin.topmarket.ui.purchase.cart
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import ir.jatlin.topmarket.R
+import ir.jatlin.topmarket.databinding.FragmentCartBinding
+import ir.jatlin.topmarket.ui.util.viewBinding
 
 class CartFragment : Fragment(R.layout.fragment_cart) {
+
+    private val viewModel by viewModels<CartViewModel>()
+    private val binding by viewBinding(FragmentCartBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        collectUiStates()
+    }
+
+    private fun initViews() {
+        binding.cartOrderItems
+    }
+
+    private fun collectUiStates() {
+
+    }
 }
---
+++ app/src/main/java/ir/jatlin/topmarket/ui/purchase/cart/CartViewModel.kt
@@ -0,0 +1,11 @@
+package ir.jatlin.topmarket.ui.purchase.cart
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(
+
+) : ViewModel() {
+}
