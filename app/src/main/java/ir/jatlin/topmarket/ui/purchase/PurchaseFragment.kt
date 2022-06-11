11c3b93b404747163a0414ffa2a62cd785f2971e
MENasri23 <m.e26159487@gmail.com>
2022-06-11 01:46:03 -0700

    feat(purchase-view): Create new fragment to purchase order items

---
+++ app/src/main/java/ir/jatlin/topmarket/ui/purchase/PurchaseFragment.kt
@@ -0,0 +1,9 @@
+package ir.jatlin.topmarket.ui.purchase
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import ir.jatlin.topmarket.R
+
+@AndroidEntryPoint
+class PurchaseFragment : Fragment(R.layout.fragment_purshase) {
+}
