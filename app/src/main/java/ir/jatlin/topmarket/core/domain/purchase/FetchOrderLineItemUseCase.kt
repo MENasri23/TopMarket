0efae5aa08b06ebc6fe5177f4d7a691e738816e9
MENasri23 <m.e26159487@gmail.com>
2022-06-12 06:58:06 -0700

    fix: Use the abstract order line item repository as a property type

--- app/src/main/java/ir/jatlin/topmarket/core/domain/purchase/FetchOrderLineItemUseCase.kt
+++ app/src/main/java/ir/jatlin/topmarket/core/domain/purchase/FetchOrderLineItemUseCase.kt
@@ -1,7 +1,7 @@
 package ir.jatlin.topmarket.core.domain.purchase
 
 import ir.jatlin.topmarket.core.data.di.IODispatcher
-import ir.jatlin.topmarket.core.data.repository.orderlineitem.DefaultOrderLineItemRepository
+import ir.jatlin.topmarket.core.data.repository.orderlineitem.OrderLineItemRepository
 import ir.jatlin.topmarket.core.domain.FlowUseCase
 import ir.jatlin.topmarket.core.model.order.OrderLineItem
 import ir.jatlin.topmarket.core.shared.fail.ErrorHandler
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class FetchOrderLineItemUseCase @Inject constructor(
-    private val orderLineItemRepository: DefaultOrderLineItemRepository,
+    private val orderLineItemRepository: OrderLineItemRepository,
     errorHandler: ErrorHandler,
     @IODispatcher dispatcher: CoroutineDispatcher
 ) : FlowUseCase<Int, OrderLineItem?>(errorHandler, dispatcher) {
